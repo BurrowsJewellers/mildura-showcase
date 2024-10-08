aba6dcd58aba95090a310f3847dfd7fa67398107
Amandeep Singh <amandeep@192.168.1.10>
2024-10-08 10:13:12 +0530

    Fixed scheduler

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,27 @@
 <?php
 
+use App\Console\Commands\EWeb\GetBrandsFromEWeb;
+use App\Console\Commands\EWeb\GetProductsFromEWeb;
+use App\Console\Commands\Shopify\GetProducts;
+use App\Console\Commands\Shopify\UploadImages;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+
+Schedule::command(GetBrandsFromEWeb::class)->daily();
+
+Schedule::command(UploadImages::class)->everyThreeHours();
+
+Schedule::command(GetProductsFromEWeb::class)->everyFifteenMinutes()->after(function(){
+    Artisan::call('shopifyUpdateInventory');
+    Artisan::call('shopifyUpdatePrice');
+});
+
+Schedule::command(GetProducts::class)->cron("5 */4 * * *")->after(function () {
+    Artisan::call('shopifyCreateProduct');
+});
